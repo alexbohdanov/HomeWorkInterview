85951f1009a9e9e2814d8d408047984f564114b4
alexbohdanov <83947461+alexbohdanov@users.noreply.github.com>
2022-03-07 19:19:14 +1100

    Add files via upload

---
+++ InterviewQuestionTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+
+
+
+class InterviewQuestionTest {
+    @Test
+    void queryServer() {
+    }
+
+
+
+
+    @Test
+    void userLogin() {
+
+
+
+    }
+    @Test
+    void userLogout() {
+
+
+    }
+    @Test
+    void adminSettings(){
+
+    }
+    @Test
+    void addProductToCart(){
+
+    }
+    @Test
+    void clearCart(){
+
+    }
+    @Test
+    void getCartDetails(){
+
+    }
+}
